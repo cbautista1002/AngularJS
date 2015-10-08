dcd272dd9f5f1146b249b9757c18ea9ec753cc1c
Carlos Bautista <cbautista@ise.com>
2015-10-08 02:02:02 +0000

    Add ctrl function for clearing

--- StudentGradeCalculator/controller.js
+++ StudentGradeCalculator/controller.js
@@ -6,4 +6,11 @@ angular.module('StudentGradeCalculator', [])
   var self = this;
   self.student = new Student();
 
+  // We need this function so that this controller can
+  // clear the input textboxes
+  self.addAssignment = function(){
+    self.student.addAssignment(self.asgn_name, self.asgn_score);
+    self.asgn_name = '';
+    self.asgn_score = '';
+  }
 });
